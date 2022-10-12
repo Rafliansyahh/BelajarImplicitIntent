edd4e413d3ac131077904bbd32b8df97ce7ee07e
Rafliansyahh <rafli.ansyahh@mhs.mdp.ac.id>
2022-10-12 17:07:10 +0700

    Projectt

--- app/src/main/java/com/rafli/belajarimplicitintent/MainActivity.java
+++ app/src/main/java/com/rafli/belajarimplicitintent/MainActivity.java
@@ -2,13 +2,63 @@ package com.rafli.belajarimplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_url, et_lokasi, et_teks;
+    Button btnBukawebsite, btnBukalokasi, btnBagikanTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_url          =findViewById(R.id.et_url);
+        et_lokasi       =findViewById(R.id.et_lokasi);
+        et_teks         =findViewById(R.id.et_teks);
+        btnBukawebsite  =findViewById(R.id.btn_buka_website);
+        btnBukalokasi   =findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks  =findViewById(R.id.btn_bagikan_teks);
+
+        btnBukawebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlWebsite = et_url. getText().toString();
+
+                Uri uriUrlWebsite = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriUrlWebsite);
+                try {
+                    startActivity(intentWebsite);
+                }catch (Exception e){
+                    et_url.setError("URL Tidak Sesuai!");
+                }
+
+
+
+
+
+            }
+        });
+
+        btnBukalokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
